4cc3d601052aa304a1c3080f9dc7411659545e3f
Guilherme Moreira <ceschiatti@gmail.com>
2016-05-28 20:07:23 -0300

    All tests working now.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,25 +1,28 @@
 import {
-  it,
-  inject,
-  describe,
-  beforeEachProviders,
+    it,
+    inject,
+    describe,
+    beforeEachProviders,
 } from '@angular/core/testing';
 
 // Load the implementations that should be tested
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
+import {ApiService} from '../shared/api.service';
 
 describe('Home', () => {
-  // provide our implementations or mocks to the dependency injector
-  beforeEachProviders(() => [
-    HomeComponent
-  ]);
+    // provide our implementations or mocks to the dependency injector
+    beforeEachProviders(() => [
+        HomeComponent,
+        ApiService
 
-  it('should log ngOnInit', inject([HomeComponent], (home) => {
-    spyOn(console, 'log');
-    expect(console.log).not.toHaveBeenCalled();
+    ]);
 
-    home.ngOnInit();
-    expect(console.log).toHaveBeenCalledWith('Hello Home');
-  }));
+    it('should log ngOnInit', inject([HomeComponent], (home) => {
+        spyOn(console, 'log');
+        expect(console.log).not.toHaveBeenCalled();
+
+        home.ngOnInit();
+        expect(console.log).toHaveBeenCalledWith('Hello Home');
+    }));
 
 });
